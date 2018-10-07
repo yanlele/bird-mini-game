6d18f92dd1b504c8bb3c0ba106541db948392512
yanlele <331393627@qq.com>
2018-10-07 10:44:26 +0800

    [+]基于es6实现的单利模式

--- Main.js
+++ Main.js
@@ -4,13 +4,16 @@
  * create time 2018-10-07 10:16
  */
 import {ResourceLoader} from "./js/base/ResourceLoader.js";
+import {Director} from "./js/Director.js";
 
 export class Main {
     constructor() {
         this.canvas = document.getElementById('game_canvas');
         this.ctx = this.canvas.getContext('2d');
         const loader = ResourceLoader.create();
-        loader.onLoaded(map=>this.onResourceFirstLoaded(map))
+        loader.onLoaded(map=>this.onResourceFirstLoaded(map));
+
+        Director.getInstance();
     }
 
     onResourceFirstLoaded(map) {
--- js/Director.js
+++ js/Director.js
@@ -5,5 +5,14 @@
  */
 
 export class Director {
+    constructor() {
+        console.log('构造器初始化');
+    }
 
+    static getInstance() {
+        if(!Director.instance) {
+            Director.instance = new Director();
+        }
+        return Director.instance;
+    }
 }
