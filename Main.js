0aef5fcc30547b1ea67226201201d27554ce9551
yanlele <331393627@qq.com>
2018-10-07 10:52:33 +0800

    [+]利用canvas实现绘图

--- Main.js
+++ Main.js
@@ -13,7 +13,22 @@ export class Main {
         const loader = ResourceLoader.create();
         loader.onLoaded(map=>this.onResourceFirstLoaded(map));
 
-        Director.getInstance();
+        let image = new Image();
+        image.src = '../res/background.png';
+
+        image.onload = ()=> {
+            this.ctx.drawImage(
+                image,
+                0,
+                0,
+                image.width,
+                image.width,
+                0,
+                0,
+                image.width,
+                image.height
+            )
+        }
     }
 
     onResourceFirstLoaded(map) {
