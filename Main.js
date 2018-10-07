3eaa8f184b0ad714a89b84050252dbde23c66ef9
yanlele <331393627@qq.com>
2018-10-07 10:41:37 +0800

    [+]资源加载

--- Main.js
+++ Main.js
@@ -3,9 +3,17 @@
  * connect me 331393627@qq.com
  * create time 2018-10-07 10:16
  */
+import {ResourceLoader} from "./js/base/ResourceLoader.js";
 
 export class Main {
     constructor() {
-        console.log('我执行了')
+        this.canvas = document.getElementById('game_canvas');
+        this.ctx = this.canvas.getContext('2d');
+        const loader = ResourceLoader.create();
+        loader.onLoaded(map=>this.onResourceFirstLoaded(map))
+    }
+
+    onResourceFirstLoaded(map) {
+        console.log(map)
     }
 }
--- js/base/Resources.js
+++ js/base/Resources.js
@@ -4,6 +4,11 @@
  * create time 2018-10-07 10:08
  */
 
-export class Resources {
-
-}
+export const Resources = [
+    ['background', 'res/background.png'],
+    ['land', 'res/land.png'],
+    ['pencilUp', 'res/pie_up.png'],
+    ['pencilDown', 'res/pie_down.png'],
+    ['birds', 'res/birds.png'],
+    ['startButton', 'res/start_button.png']
+];
