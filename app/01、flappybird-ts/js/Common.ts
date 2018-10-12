19a6138fda746b8835a6e2d43a19bf3abc38c299
yanlele <331393627@qq.com>
2018-10-12 11:26:54 +0800

    [+]初始化项目完成

--- app/01、flappybird-ts/js/Common.ts
+++ app/01、flappybird-ts/js/Common.ts
@@ -1,19 +1,22 @@
 /**
  * create by yanlele
- * create time 2018-10-12 10:57
+ * create time 2018-10-12 11:23
  */
 
-
-class Main {
+/**
+ * 主要是用来对基本的一些内容的控制，比如canvas的大小之类的
+ */
+class Common {
     private canvas;
     private ctx;
 
     constructor() {
-        this.canvas = document.getElementById('canvas');
+        this.canvas = document.getElementById('game_canvas');
         this.ctx = this.canvas.getContext('2d');
         this.canvas.setAttribute('width', window.innerWidth);
         this.canvas.setAttribute('height', window.innerHeight);
     }
+
 
     onResize() {
         window.onresize = () => {
@@ -22,11 +25,4 @@ class Main {
             this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
         }
     }
-
-    run() {
-
-    }
 }
-
-const main: Main = new Main();
-main.onResize();
